9ba85bc3debb7cf2192ef261f4b46e11f2595a47
Cory <Coryjh85@gmail.com>
2017-06-01 11:30:11 -0700

    Got Select menu working for register.component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -20,19 +20,28 @@ import {
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   colonist: Colonist;
+  jobs: Job[] = [];
+  NO_JOB_SELECTED = '(none)';
 
-  constructor(private jobService: JobsService, private colonistService: ColonistService) {}
+  constructor(private jobService: JobsService, private colonistService: ColonistService, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+
+    this.jobService.getData()
+        .subscribe((data) => {
+        this.jobs = data.jobs;
+      });
+
     this.registerForm = new FormGroup({
       name: new FormControl('', [
         Validators.required,
         Validators.maxLength(100),
         Validators.minLength(3)]),
-
       age: new FormControl('', [Validators.required]),
-      job_id: new FormControl('', [])
+      job_id: new FormControl(this.NO_JOB_SELECTED, [])
     });
+
 }
 
 }
+
