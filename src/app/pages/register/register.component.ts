dac2b5dfff94362d5b59d099aa7a82e14a9e2931
Cory <Coryjh85@gmail.com>
2017-06-01 10:25:59 -0700

    bound form control group

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,6 +3,13 @@ import { Job } from '../../models/job';
 import { Colonist } from '../../models/colonist';
 import { JobsService } from '../../services/jobs.service';
 import { ColonistService } from '../../services/colonist.service';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder, Validators,
+  ValidatorFn,
+  AbstractControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,10 +18,21 @@ import { ColonistService } from '../../services/colonist.service';
   providers: [JobsService, ColonistService]
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
   colonist: Colonist;
 
   constructor(private jobService: JobsService, private colonistService: ColonistService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(100),
+        Validators.minLength(3)]),
+
+      age: new FormControl('', [Validators.required]),
+      job_id: new FormControl('', [])
+    });
+}
 
 }
