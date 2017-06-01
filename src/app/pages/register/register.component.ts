9934055f623433f8e23c589f3ec94a1ed9e69e2d
Cory <Coryjh85@gmail.com>
2017-06-01 10:05:55 -0700

    imported form tools into register component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -11,23 +11,10 @@ import { ColonistService } from '../../services/colonist.service';
   providers: [JobsService, ColonistService]
 })
 export class RegisterComponent implements OnInit {
-  jobs: Job[] = [];
-  colonistName: string;
-  colonistAge: string;
-  colonistJobId: string = 'no job';
-  public colonists: Colonist;
+  colonist: Colonist;
 
   constructor(private jobService: JobsService, private colonistService: ColonistService) {}
 
-  ngOnInit() {
-    this.jobService.getData()
-      .subscribe((data) => {
-        this.jobs = data.jobs;
-      });
-  }
-  postColonist(){
-    const colonist = new Colonist(this.colonistName, this.colonistAge, this.colonistJobId);
-               this.colonistService.postData(colonist)
-                        .subscribe((newColonist) => {})
-  }
+  ngOnInit() {}
+
 }
